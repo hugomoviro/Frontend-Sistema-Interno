b8d33d4f9be2853b0855c18e83b1171ad38c46d2
Hugo Andres Martinez Guzman <hugo.martinez@moviro.mx>
2023-08-28 13:51:27 -0600

    Routing Module

--- src/app/routing/routing/routing.module.ts
+++ src/app/routing/routing/routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
 
-
+const routes : Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'empresa', loadChildren: () => import('./../../OrganizacionApi/Empresas/empresa.module').then(m => m.EmpresaModule)},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+
 })
 export class RoutingModule { }
